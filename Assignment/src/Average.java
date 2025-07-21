6e515855dcfb569412d673cca8d1de8ff0957fce
saurav2327 <acesaurav444@gmail.com>
2025-07-21 16:35:00 +0530

    change

--- Assignment/src/Average.java
+++ Assignment/src/Average.java
@@ -1,6 +1,7 @@
 public class Average {
     public static void main(String[] args) {
         int average = (4+5+6+7)/4;
+        System.out.print("average is as: ");
         System.out.println(average);
     }
 }
